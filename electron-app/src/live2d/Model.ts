f304d23c139a7dc2a010b918e423094c3431850e
Ranko Hata <san332627946@gmail.com>
2026-06-04 23:54:56 +0800

    fix: bind textures to renderer via r.bindTexture(i, texId)

--- electron-app/src/live2d/Model.ts
+++ electron-app/src/live2d/Model.ts
@@ -145,6 +145,8 @@ export class Model extends CubismUserModel {
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
         gl.bindTexture(gl.TEXTURE_2D, null);
+        // Register texture with renderer
+        if (texId) r.bindTexture(i, texId);
       }
     }
 
